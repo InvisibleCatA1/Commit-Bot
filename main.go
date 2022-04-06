fdc7038cf710bfcbdf2d7897e772bdfe0ac583c3
shiel <shieldscoen@gmail.com>
2022-04-06 12:50:10 -0600

    l

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"time"
 
 	"github.com/TwiN/go-color"
 )
@@ -16,9 +18,7 @@ var error = "[" + color.Red + "E" + color.Reset + "] "
 var info = "[" + color.Green + "I" + color.Reset + "] "
 
 func main() {
-	var dir string
-
-	dir = input(userIn + "Enter the directory that the commit bot will use: ")
+	dir := input(userIn + "Enter the directory that the commit bot will use: ")
 
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		fmt.Print(error + "That dir does not exist")
@@ -37,21 +37,36 @@ func main() {
 				run = false
 			} else if cmd == "commit" {
 				commitMsg = input(userIn + "Print the message for commit (can not be nothing): ")
-				cmd := exec.Command("git", "commit", "-a", "-m", commitMsg)
-				var outb, errb bytes.Buffer
-				cmd.Stdout = &outb
-				cmd.Stderr = &errb
-				err := cmd.Run()
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(outb.String())
-				push()
+				commit(commitMsg)
+			} else if cmd == "help" {
+				fmt.Println(info + "commit - commit to a git repo (muse already be initalized)")
+				fmt.Println(info + "quit - exists the program")
+				fmt.Println(info + "help - displays this page")
+				fmt.Println(info + "regular commit - commits every minute (delay can be specified)")
+			} else if cmd == "regular commit" {
+				times := input(userIn + "Number of times to commit: ")
+				delay := input(userIn + "delay between commits (minutes): ")
+				for i := 0; i < strToInt(times); i++ {
+					commit("Repeat commit")
+					fmt.Println(info + " Waiting " + delay + " minutes unitl next commit")
+					time.Sleep(time.Duration(strToInt(delay)) * time.Minute)
 
+				}
+
+			} else {
+				fmt.Println(error + "Unkown command: " + cmd + "; type help for list of commands")
 			}
 		}
 	}
 }
+func strToInt(val string) int {
+	intVal, err := strconv.Atoi(val)
+	if err == nil {
+		fmt.Println(err)
+	}
+	return intVal
+}
+
 func push() {
 	cmd := exec.Command("git", "push")
 	var outb, errb bytes.Buffer
@@ -62,6 +77,22 @@ func push() {
 		log.Fatal(err)
 	}
 	fmt.Println(info + "Pushed")
+}
+func commit(commitMsg string) {
+	if commitMsg == "" {
+		fmt.Println(error + "commit msg cannot be none")
+		return
+	}
+	cmd := exec.Command("git", "commit", "-a", "-m", commitMsg)
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(outb.String())
+	push()
 }
 func input(prompt string) string {
 	fmt.Print(prompt)
